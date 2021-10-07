641c5e9f1c2922c42f2347fc5931a44d6bb1d0b5
jasi111 <jasimaha@gmail.com>
2021-10-07 04:52:19 +0400

    removed env

--- src/api/api.js
+++ src/api/api.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL: process.env.REACT_APP_API
+    // baseURL: process.env.REACT_APP_API
+    baseURL: "https://dog.ceo/api"
 })
